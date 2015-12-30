cb4131f04947a1f5b67f900ae4216546132a600a
qubaish-techverx <qubaish@gems.techverx.com>
2015-12-30 13:52:33 +0500

    update config file

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'techverx');
+define('DB_NAME', 'staging_techverx');
 
 /** MySQL database username */
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'T3chVerxDb2015!');
 
 /** MySQL hostname */
-define('DB_HOST', 'local.phsq-commerce.com');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
