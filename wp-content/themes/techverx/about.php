7f1fdb2fd8075faf2fbad6823a81209c31a57ffb
qubaish-techverx <qubaish@gems.techverx.com>
2016-01-07 20:03:42 +0500

    update content on about page

--- wp-content/themes/techverx/about.php
+++ wp-content/themes/techverx/about.php
@@ -75,13 +75,37 @@ get_header();
             <img src="<?php bloginfo('template_directory'); ?>/images/rao-mohsin-raza.gif" class="img-responsive center-block img-rounded brd-red">
             <h3>RAO MOHSIN RAZA</h3>
             <h4>Founder</h4>
-            <p class="text-justify">We have a remarkably low turnover rate, because we believe there is no point in doing something if it’s not fun. Great products turn us on and we are always striving to one up ourselves by staying on the cutting edge of technology and approaches, because our biggest competition is ourselves.</p>
+            <p class="text-justify">A software engineer by qualification and a corporate HR professional in another life, in reality Rao 
+
+Mohsin Raza is a natural born entrepreneur. Techverx is his baby; born in his laptop at his home, 
+
+he has led it to the dynamic, global software development company it is today with a multistory 
+
+campus and a team of hundreds. With over 20 years of IT and management experience, Rao is a 
+
+very hands-on CEO, personally involved with every project and ensuring that his clients always 
+
+get the best return on their investment. Rao is also a recognized expert on global outsourcing and 
+
+an IAOP Certified Outsourcing Professional.</p>
         </div>
         <div class="col-md-6">
             <img src="<?php bloginfo('template_directory'); ?>/images/peter.gif" class="img-responsive center-block img-rounded brd-red">
             <h3>PETER J. SPERRY</h3>
             <h4>Co-Founder</h4>
-            <p class="text-justify">We have a remarkably low turnover rate, because we believe there is no point in doing something if it’s not fun. Great products turn us on and we are always striving to one up ourselves by staying on the cutting edge of technology and approaches, because our biggest competition is ourselves.</p>
+            <p class="text-justify">A software veteran with over 15 years of global experience as a developer and CTO 
+
+in various industries, Peter is a holistic solution oriented thinker and our not-so-
+
+secret weapon when it comes to truly innovative yet simple and elegant ideas. Peter 
+
+manages our development teams in the US, Europe and Hong Kong and is an active 
+
+part of all our projects, providing solutions and guidance at every step. His 
+
+specialties include database, marketing technologies, ETL & data hygiene, 
+
+management systems and 3rd party integrations.</p>
         </div>
     </div>
 
@@ -92,19 +116,49 @@ get_header();
             <img src="<?php bloginfo('template_directory'); ?>/images/umar.gif" class="img-responsive center-block img-rounded brd-red">
             <h3>UMAR KHAN BURKI</h3>
             <h4>Head of Sales</h4>
-            <p class="text-justify">We have a remarkably low turnover rate, because we believe there is no point in doing something if it’s not fun. Great products turn us on and we are always striving to one up ourselves by staying on the cutting edge of technology and approaches, because our biggest competition is ourselves.</p>
+            <p class="text-justify">Umar years of sales experienced, having worked in retail and software industries. 
+
+He specializes in B2B sales and account management, personally involved with all 
+
+projects and ensuring that his team delivers nothing short of the best possible 
+
+service to the clients.</p>
         </div>            
         <div class="col-md-4">
             <img src="<?php bloginfo('template_directory'); ?>/images/usman.gif" class="img-responsive center-block img-rounded brd-red">
             <h3>USMAN KHAN</h3>
             <h4>Princiapal Software Architect</h4>
-            <p class="text-justify">We have a remarkably low turnover rate, because we believe there is no point in doing something if it’s not fun. Great products turn us on and we are always striving to one up ourselves by staying on the cutting edge of technology and approaches, because our biggest competition is ourselves.</p>            
+            <p class="text-justify">A highly energetic and self motivated team leader, Usman has over 10 years of
+
+software engineering experience. He is known for his ability to validate and design 
+
+solutions either from a soup to nuts situation or migrating from existing complexity 
+
+to a predictable, transparent framework. One of his most fundamental roles is to 
+
+ensure fast, predictable, and uninterrupted flow of planned work that delivers 
+
+maximum value to the client, while minimizing disruption and impact of unplanned 
+
+work, for stable, predictable, and trustworthy services.</p>            
         </div>
         <div class="col-md-4">
             <img src="<?php bloginfo('template_directory'); ?>/images/faraz.gif" class="img-responsive center-block img-rounded brd-red">
             <h3>FARAZ MALIK</h3>
             <h4>Chief Creative Officer</h4>
-            <p class="text-justify">We have a remarkably low turnover rate, because we believe there is no point in doing something if it’s not fun. Great products turn us on and we are always striving to one up ourselves by staying on the cutting edge of technology and approaches, because our biggest competition is ourselves.</p>            
+            <p class="text-justify">With over a decade of combined experienced in advertising and broadcast media, 
+
+Faraz is a seasoned storyteller and marketing expert with creativity and art 
+
+embedded in his DNA. He has worked in leading multinational ad agencies, creating 
+
+hundreds of campaigns for global brands like Pepsi Cola and Nestle. He brings this 
+
+wealth of marketing experience and unique creative vision to Techverx as CCO, 
+
+responsible for the look and overall user experience of every product and interface 
+
+that we produce.</p>            
         </div>
     </div>
 
